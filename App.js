9721d13113d5705d18e0f4d35005ea39d44383ad
HoangManhh <manhnh.b21cn514@stu.ptit.edu.vn>
2025-02-25 20:58:19 +0700

    add Todo List

--- App.js
+++ App.js
@@ -1,35 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, ScrollView, View, Button } from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+  function textChange(text){
+    setTask(text);
+  }
+  function addTask(){
+    setTaskList((currentTaskList) => [...currentTaskList, task]);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Type your name </Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: 200,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        defaultValue="Name"
-      />
-      <Button title='Submit'/>
-      <Text>Type your name </Text>
-      <Text>Type your name </Text>
-      <Text>Type your name </Text>
-      <Text>Type your name </Text>
-      <Text>Type your name </Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.searchView}>
+        <TextInput onChangeText={textChange} style={styles.searchBox} placeholder='Your task'></TextInput>
+        <Button onPress={addTask} title="Add" />
+      </View>
+      <View style={styles.taskList}>
+        {taskList.map((task, key) => <Text key={key}>{task}</Text>)}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    padding: 10,
+    marginBottom: 10,
   },
+  searchView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+
+  },
+  searchBox: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+  taskList: {
+    padding: 10,
+  }
 });
